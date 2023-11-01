786a08fd6785acb1f7fb75e3a92583cef21fea85
Simon <simon.sokolowski@outlook.com>
2023-11-01 20:30:07 +0000

    test

---
+++ labContent.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int cube();
+void askUserForCube();
+
+int main() {
+    askUserForCube();
+    return 0;
+}
+
+int cube(int userInput) {
+    return userInput * userInput * userInput;
+}
+
+void askUserForCube() {
+    std::cout << "input number you would like to cube" << std::endl;
+
+    int input;
+
+    std::cin >> input;
+
+    std::cout << input << " cubed is " << cube(input);
+}
