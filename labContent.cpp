cfd6f1550f0867048e188a1c9d2786503eea7bdd
Simon <simon.sokolowski@outlook.com>
2023-11-01 20:32:13 +0000

    cube function

--- labContent.cpp
+++ labContent.cpp
@@ -16,7 +16,6 @@ void askUserForCube() {
     std::cout << "input number you would like to cube" << std::endl;
 
     int input;
-
     std::cin >> input;
 
     std::cout << input << " cubed is " << cube(input);
